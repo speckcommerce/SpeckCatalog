450f11802cd6051133ae3af339a28fc358034bfd
Nigel Lundsten <nigel.lundsten@gmail.com>
2011-12-06 12:34:26 -0700

    Removed static dependency on edpuser, commented out incomplete code

--- src/Management/Controller/IndexController.php
+++ src/Management/Controller/IndexController.php
@@ -8,8 +8,7 @@ class IndexController extends ActionController
 {
     public function indexAction()
     {
-        die('management/indexcontroller/indexaction');
-        $user = $this->getLocator()->get('edpuser_user_service')->getAuthService()->getIdentity();
+        $user = $this->getLocator()->get('speckcatalog_user_service')->getAuthService()->getIdentity();
         return array('session' => $this->getLocator()->get('catalog_management_service')->getSession($user));
     }
 }

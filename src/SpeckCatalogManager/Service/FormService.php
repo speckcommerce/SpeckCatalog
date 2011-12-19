0aafd3418a17f58c7809023a07411eef266f55e1
nigel lundsten <nigel.lundsten@gmail.com>
2011-12-19 14:17:59 -0700

    still working on views, added more tabs to the product, worked out some stuff on how to display productuoms

--- src/SpeckCatalogManager/Service/FormService.php
+++ src/SpeckCatalogManager/Service/FormService.php
@@ -5,8 +5,6 @@ class FormService
     protected $forms = array();
     protected $locator;
 
-
-
     public function __construct($locator)
     {
         $this->locator = $locator;
@@ -17,12 +15,22 @@ class FormService
         
         $this->forms['blank']['choice'] = $this->getFormManager(null, 'Choice')->getForm();
         $this->forms['blank']['option-radio'] = $this->getFormManager(null, 'Option')->getForm();
+        $this->forms['blank']['item-uom'] = $this->getFormManager(null, 'ItemUom')->getForm();
 
         $this->forms['form'] = $this->getFormManager($product)->getForm();
         $returnedItem = $product->getItem();
         if($returnedItem){
             $this->forms['item']['form'] = $this->getFormManager($returnedItem)->getForm();
+            $returnedItemUoms = $returnedItem->getUoms();
+            if($returnedItemUoms){
+                $itemUoms = array();
+                foreach($returnedItemUoms as $itemUom){
+                    $itemUoms[$itemUom->getItemUomId()]['form'] = $this->getFormManager($itemUom)->getForm();
+                }
+                $this->forms['item']['item-uoms'] = $itemUoms;
+            }
         }
+        
         $returnedOptions = $product->getOptions();
         if($returnedOptions){
             $options = array();
@@ -42,16 +50,15 @@ class FormService
         return $this->forms;
     }
 
-
-
-
-
     private function getFormManager($entity=null, $className=null)
     {
         if(!$className){ 
             $class = explode('\\', get_class($entity)); $className = array_pop($class);  
         }
         $definitionClass = 'SpeckCatalog\Form\Definition\\'.$className;
+        if(!class_exists($definitionClass)){
+            die("sorry, dont have that definition class - {$className}, couldnt get your formManager");
+        }
         return $this->locator->get('spiffy_form', array('definition' => $definitionClass, 'data' => $entity,))->build();
     }      
 }

ba5f9c6e942d8e65577ac4148886b66efc3d1a2a
Nigel Lundsten <nigel.lundsten@gmail.com>
2012-03-20 13:44:09 -0700

    work on category model, and view partial & form

--- src/Catalog/Model/Category.php
+++ src/Catalog/Model/Category.php
@@ -3,6 +3,7 @@ namespace Catalog\Model;
 class Category extends ModelAbstract
 {
     protected $categoryId;
+    protected $products = array();
  
     public function getCategoryId()
     {
@@ -18,5 +19,23 @@ class Category extends ModelAbstract
     public function getId()
     {
         return $this->getCategoryId();
+    }
+ 
+    public function getProducts()
+    {
+        return $this->products;
+    }
+ 
+    public function setProducts($products)
+    {
+        $this->products = $products;
+        return $this;
+    }
+
+    public function hasProducts()
+    {
+        if(count($this->getProducts()) > 0){
+            return true;
+        }
     }
 }

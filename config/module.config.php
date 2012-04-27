165c3800b37391895afa3398d12d9348b5803a27
Nigel Lundsten <nigel.lundsten@gmail.com>
2012-04-27 10:37:07 -0700

    merge and return config files

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,18 @@
 <?php
-return array(
+$config = array(
     'speck_catalog' => array(
         'installer' => 'Catalog\Service\Installer',
     )
 );
+
+$configFiles = array(
+    __DIR__ . '/module.config.di.php',
+    __DIR__ . '/module.config.di.alias.php',
+    __DIR__ . '/module.config.di.routes.php',
+);
+
+foreach($configFiles as $configFile) {
+    $config = Zend\Stdlib\ArrayUtils::merge($config, include $configFile);
+}
+
+return $config;

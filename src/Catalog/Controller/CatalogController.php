cb5359ffb2ea5cb2513b2eb65b16740f847d8b16
nigel lundsten <nigel.lundsten@gmail.com>
2012-06-26 11:17:42 -0700

    fix product shortcut, and add link in product view from catalogmanager

--- src/Catalog/Controller/CatalogController.php
+++ src/Catalog/Controller/CatalogController.php
@@ -20,14 +20,14 @@ class CatalogController extends ActionController
     {
         $id = $this->getEvent()->getRouteMatch()->getParam('id');
         $product = $this->getCatalogService()->getModel('product', $id);
-        
+
         return new ViewModel(array('product' => $product));
     }
-    
+
     public function productRedirectAction()
     {
         $id = (int) $this->getEvent()->getRouteMatch()->getParam('id');
-        $this->redirect()->toRoute('catalog/product', array('id' => $id));
+        return $this->redirect()->toRoute('catalog/product', array('id' => $id));
     }
 
     public function categoryAction()
@@ -35,8 +35,8 @@ class CatalogController extends ActionController
         $id = $this->getEvent()->getRouteMatch()->getParam('id');
         $category = $this->getCatalogService()->getModel('category', $id);
         var_dump($category);
-    }   
-    
+    }
+
     public function getCatalogService()
     {
         if(null === $this->catalogService){
@@ -44,21 +44,21 @@ class CatalogController extends ActionController
         }
         return $this->catalogService;
     }
- 
+
     public function setCatalogService($catalogService)
     {
         $this->catalogService = $catalogService;
         return $this;
     }
- 
+
     public function getModelLinkerService()
     {
         return $this->modelLinkerService;
     }
- 
+
     public function setModelLinkerService($modelLinkerService)
     {
         $this->modelLinkerService = $modelLinkerService;
         return $this;
     }
-}   
+}

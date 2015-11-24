36492c6fb894dcfd64045bd7abf73a28062ff54e
Aleksey Khudyakov <aleksey@xerkus.pro>
2015-11-24 18:50:15 +1000

    Bump minimum php version, update some ci configs

--- .travis.install.sh
+++
@@ -1,9 +0,0 @@
-set -x
-if [ "$TRAVIS_PHP_VERSION" = 'hhvm' ] || [ "$TRAVIS_PHP_VERSION" = 'hhvm-nightly' ] ; then
-    curl -sS https://getcomposer.org/installer > composer-installer.php
-    hhvm composer-installer.php
-    hhvm -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 composer.phar update
-else
-    composer self-update
-    composer update
-fi
